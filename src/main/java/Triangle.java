aaaf3f9ff98b9d31483a4225c77e6e2007129592
Romanx <mcauliffe.alex@gmail.com>
2014-05-01 13:11:14 +0100

    Created triangle class

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Alex on 01/05/2014.
+ */
+public class Triangle {
+    private final float a, b, c;
+    private static final int MAX_SIZE = 200;
+
+    public Triangle(float a, float b, float c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
