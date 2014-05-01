ffc484366119e2446d84c944c3ceb8a3e3979e6b
Romanx <mcauliffe.alex@gmail.com>
2014-05-01 15:06:07 +0100

    Created MainTest class for testing main

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Created by Alex on 01/05/2014.
+ */
+@RunWith(JUnit4.class)
+public class MainTest {
+}
