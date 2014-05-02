cda454867ae853cf2c6ed51e0749210aa9760269
Romanx <mcauliffe.alex@gmail.com>
2014-05-02 17:39:00 +0100

    Added a rule for JUnit and JMock mocking

--- src/test/java/PublisherTest.java
+++ src/test/java/PublisherTest.java
@@ -1,5 +1,11 @@
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
+
 /**
  * Created by Alex on 02/05/2014.
  */
 public class PublisherTest {
+    @Rule
+    public JUnitRuleMockery context = new JUnitRuleMockery();
+
 }
