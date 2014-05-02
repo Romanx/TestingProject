502f3e9275ceef689db3f60e92eeecb45bc831a9
Romanx <mcauliffe.alex@gmail.com>
2014-05-02 17:42:47 +0100

    Wrote single subscriber test #4

--- src/test/java/PublisherTest.java
+++ src/test/java/PublisherTest.java
@@ -1,5 +1,9 @@
+import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
+import org.junit.Test;
+import uk.co.diggydiggyhole.testingproject.integration.ISubscriber;
+import uk.co.diggydiggyhole.testingproject.integration.Publisher;
 
 /**
  * Created by Alex on 02/05/2014.
@@ -8,4 +12,19 @@ public class PublisherTest {
     @Rule
     public JUnitRuleMockery context = new JUnitRuleMockery();
 
+    @Test
+    public void singleSubscriberRecievesMessage() {
+        final ISubscriber subscriber = context.mock(ISubscriber.class);
+
+        Publisher p = new Publisher();
+        p.addSubscriber(subscriber);
+
+        final String message = "Hello World";
+
+        context.checking(new Expectations() {{
+            oneOf (subscriber).recieveMessage(message);
+        }});
+
+        p.sendMessage(message);
+    }
 }
