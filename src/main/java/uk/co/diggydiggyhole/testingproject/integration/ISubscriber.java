b8cf5e75da3ec27dcc8a4a7635d3ce0d2912ff89
Romanx <mcauliffe.alex@gmail.com>
2014-05-02 16:59:53 +0100

    Created an interface for the subscribers.

---
+++ src/main/java/uk/co/diggydiggyhole/testingproject/integration/ISubscriber.java
@@ -0,0 +1,9 @@
+package uk.co.diggydiggyhole.testingproject.integration;
+
+/**
+ * Created by Alex on 02/05/2014.
+ * Interface for defining Subscriber operations
+ */
+public interface ISubscriber {
+    void recieveMessage(String message);
+}
